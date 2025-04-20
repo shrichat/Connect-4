c4b7f15d5a2b440e7bd4f20d0956c5382c1426a7
shrichat <158341244+shrichat@users.noreply.github.com>
2025-04-20 12:03:19 -0500

    Initial project structure with client and server Maven projects

---
+++ client-ntf/src/main/java/com/ntf/client/App.java
@@ -0,0 +1,13 @@
+package com.ntf.client;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
